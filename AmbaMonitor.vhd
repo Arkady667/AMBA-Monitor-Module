6d4c2f262026ef7328ede6309bc6418fa009a370
Arkady667 <arkadiusz.czuba@gmail.com>
2017-09-18 15:13:15 +0200

    Commit #5
    v0.4 - AmbaMonitor.vhd - minor changes (syntax, functionality) - flag and outputs can generate additional mux on output - removed.

--- AmbaMonitor.vhd
+++ AmbaMonitor.vhd
@@ -10,8 +10,7 @@ entity AmbaMonitor is
 	generic(
 		pwDataWidth	 	: integer   := 32; 
 		pAddrWidth   	: integer   := 32;
-        pIndex       	: integer   := 0;
-        pMask        	: integer   := 16#FFF#
+        pIndex       	: integer   := 0
 		);
 	port( 
 		iCLK   			: in std_logic;
@@ -21,9 +20,9 @@ entity AmbaMonitor is
 		iAhbSlvIn		: in ahb_slv_out_type;
 		--iAHBMI 		: in ahb_mst_in_type;
 		--iAHBMO 		: in ahb_mst_out_type;
-		oApbSlv			: in apb_slv_out_type;
-		oError			: out std_logic;
-		oEAddr 			: out std_logic
+		oApbSlv			: out apb_slv_out_type
+		--oError			: out std_logic;
+		--oEAddr 			: out std_logic
 		);
 
 end entity;
@@ -31,16 +30,16 @@ end entity;
 
 architecture beh of AmbaMonitor is 
 	
-	type MonitorState is (stIdle,stReady_n);
-	type TApbSlaveState is (stIdle, stWriteData, stReadData);
+	type tMonitorState 	is (stIdle,stReady_n);
+	type tApbSlaveState is (stIdle, stWriteData, stReadData);
 
 	type reg_type is record
-		ApbSlaveState   : TApbSlaveState;
-		state			: MonitorState;
+		apbSlaveState   : tApbSlaveState;
+		monitorState	: tMonitorState;
 		addrFlag		: std_logic;
 		errorMessage 	: std_logic;
-		ApbInputData    : std_logic_vector(pwDataWidth-1 downto 0)
-		ApbOutputData   : std_logic_vector(pwDataWidth-1 downto 0)
+		apbInputData    : std_logic_vector(pwDataWidth-1 downto 0)
+		apbOutputData   : std_logic_vector(pwDataWidth-1 downto 0)
 		errorAddr 	 	: std_logic_vector(pAddrWidth-1 downto 0);
 		errorData    	: std_logic_vector(pwDataWidth-1 downto 0);
 		errorAddrTemp	: std_logic_vector(pAddrWidth-1 downto 0);
@@ -50,78 +49,80 @@ architecture beh of AmbaMonitor is
 	signal r, rin  : reg_type;
 
 	begin
-		COMB: process(iAhbSlvOut, iAhbSlvIn, iApbSlv, r)
+		P_COMB: process(iAhbSlvOut, iAhbSlvIn, iApbSlv, r)
 			variable v : reg_type;
 		begin
-			v := r;
-			addrFlag <= '0'
-			oApbSlv.pindex <= pIndex;
-			v.errorAddrTemp := iAhbSlvIn.haddr;
-
-			------- APB INTERFACE ---------------
-
-			case (r.ApbSlaveState) is	
-				when stIdle =>
-					if (iApbSlv.psel(pIndex) = '1') then
-						if (iApbSlv.pwrite = '1') then			--write
-	 						v.ApbSlaveState := stWriteData;
-						else 									--read
-							v.ApbSlaveState := stReadData;
-						end if;
-					end if;				 	
-				when stWriteData =>
-					v.ApbInputData := iApbSlv.pwdata;
-					v.ApbSlaveState := stIdle;
-				when stReadData => 
-					oApbSlv.prdata <= v.ApbOutputData;
-					v.ApbSlaveState := stIdle;
-				when others =>
-					v.ApbSlaveState := stIdle;
-			end case;
+			v 					:= r;
+			v.addrFlag 			<= '0'
+			oApbSlv.pindex 		<= pIndex;
+			v.errorAddrTemp 	:= iAhbSlvIn.haddr;
+			oApbSlv.prdata  	<= (others => '0');
 
 			------ AMBA MONITOR MACHINE -----------
 
-			case (r.MonitorState) is
+			case (r.monitorState) is
 				when stIdle =>
 					if (iAhbSlvIn.hready = '0') then
-						v.state := ready_n;
 						v.errorAddr := r.errorAddrTemp;
+						v.state := stReady_n;
 					end if;
 				when stReady_n =>
 					if (iAhbSlvOut.hresp = "10") then
 						v.errorMessage <= '1';
-						addrFlag <= '1'
+						--v.addrFlag <= '1'
 					else
-						state := stIdle;
+						v.state := stIdle;
 					end if;
 				when others =>
 					v.state := stIdle;
 			end case;
-			rin <= v;
-		end process COMB;
 
-		SEQ: process(iCLK)
+			------- APB INTERFACE ---------------
+
+			case (r.apbSlaveState) is	
+				when stIdle =>
+					if (iApbSlv.psel(pIndex) = '1') then
+						if (iApbSlv.pwrite = '1') then			--write
+	 						v.apbSlaveState := stWriteData;
+						else 									--read
+							v.apbSlaveState := stReadData;
+						end if;
+					end if;				 	
+				when stWriteData =>
+					v.apbInputData 	:= iApbSlv.pwdata;
+					v.apbSlaveState := stIdle;
+				when stReadData => 
+					oApbSlv.prdata  <= r.apbOutputData; --apbOutputData not used 
+					v.apbSlaveState := stIdle;
+				when others =>
+					v.apbSlaveState := stIdle;
+			end case;
+
+			rin <= v;
+		end process P_COMB;
+
+		P_SEQ: process(iCLK, iRST_n)
 		begin
-			if rising_edge(iCLK) then
+			if (iRST_n = '0') then -- asynch rst_n
+				r.errorMessage 		<= '0';
+				r.errorAddr 		<= (others => '0');
+				r.errorAddrTemp 	<= (others => '0');
+				r.errorData 		<= (others => '0');
+				r.errorDataTemp 	<= (others => '0');
+				r.ApbSlaveState		<= stIdle;
+				r.MonitorState 		<= stIdle;
+				r.ApbInputData 		<= (others => '0');
+				r.ApbOutputData 	<= (others => '0');
+			elsif rising_edge(iCLK) then
 				r <= rin;
 			end if;
-			if (iRST_n = '0') then -- asynch rst_n
-				r.errorMessage <= '0';
-				r.errorAddr <= (others => '0');
-				r.errorAddrTemp <= (others => '0');
-				r.errorData <= (others => '0');
-				r.errorDataTemp <= (others => '0');
-				r.ApbSlaveState <= stIdle;
-				r.MonitorState <= idle;
-				r.ApbInputData <= (others => '0');
-				r.ApbOutputData <= (others => '0');
-			end if;
-		end process SEQ;
-	if (addrFlag = '1') then
-		oEAddr <= r.errorAddr;
-		oError <= r.errorMessage;
-	else
-		oError <= (others => '0');
-		oEAddr <= (others => '0');
-	end if;	
+		end process P_SEQ;
+
+	--if (addrFlag = '1') then
+	--	oEAddr <= r.errorAddr;
+	--	oError <= r.errorMessage;
+	--else
+	--	oError <= (others => '0');
+	--	oEAddr <= (others => '0');
+	--end if;	
 end architecture;	
