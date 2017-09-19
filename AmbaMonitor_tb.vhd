9b5f9c53aef8342205e58ffa0919fb01cfb40d8b
Arkady667 <arkadiusz.czuba@gmail.com>
2017-09-19 17:43:17 +0200

    Commit #9
    v1.0 - AmbaMonitor and _tb are checked by VHDL check in Libero, simulation is running but dont as expected

--- AmbaMonitor_tb.vhd
+++ AmbaMonitor_tb.vhd
@@ -1,4 +1,7 @@
 
+library Cbk;
+    use Cbk.P3_pkg.all;
+    use Cbk.P3Sim_pkg.all;
 library ieee;
 	use ieee.std_logic_1164.all;
 	use ieee.numeric_std.all;
@@ -27,26 +30,25 @@ architecture sim of AmbaMonitor_tb is
 	constant	pwDataWidth	 	: integer   := 32; 
 	constant	pAddrWidth   	: integer   := 32;
     constant	pIndex       	: integer   := 0;
-    constant	pIrq         	: integer   := 0;
-    constant	pMask        	: integer   := 16#FFF#;
 	
 	signal	iCLK   			: std_logic;
 	signal	iRST_n 			: std_logic;
 	signal	iApbSlv 		: apb_slv_in_type;
-	signal	iAhbSlvOut 		: ahb_slv_in_type;
-	signal	iAhbSlvIn		: ahb_slv_out_type;
+	signal	iAhbSlvOut 		: ahb_slv_out_type;
+	signal	iAhbSlvIn		: ahb_slv_in_type;
 	signal	oApbSlv			: apb_slv_out_type;
 	signal	oError			: std_logic;
-	signal	oEAddr 			: std_logic;
+	signal	oEAddr 			: std_logic_vector(pAddrWidth-1 downto 0);
 	signal  dbgi 			: at_slv_dbg_in_type;
 	signal  dbgo 			: at_slv_dbg_out_type;
-
+	signal EndOfSimulation                   : std_logic := '0';
+begin
 	P_CLK: process
 	begin
-		iClk <= '0';
-        Wait for (ClkPeriod / 2) * ns;
-        iClk <= '1';
-        Wait for (ClkPeriod / 2) * ns;
+		iCLK <= '0';
+        wait for (ClkPeriod / 2) * ns;
+        iCLK <= '1';
+        wait for (ClkPeriod / 2) * ns;
     end process;
     ------------------------------------------------
 	-- P_STIM: Main process and test sequencer
@@ -57,19 +59,20 @@ architecture sim of AmbaMonitor_tb is
 	--variables
 	procedure ahbSlvOutputData(address    : in std_logic_vector(pAddrWidth-1 downto 0);
 							   ready	  : in std_logic;
-							   response   : in std_logic_vector(1 downto 0))
-
-		wait until rising_edge(iCLK)
-		iAhbSlvOut.haddr <= address;
-		iAhbSlvOut.hready <= ready;
-		iAhbSlvOut.hresp <= response;
+							   response   : in std_logic_vector(1 downto 0)) is
+	begin
+		wait until rising_edge(iCLK);
+		iAhbSlvIn.haddr 	<= address;
+		iAhbSlvOut.hready 	<= ready;
+		iAhbSlvOut.hresp 	<= response;
 
 	end ahbSlvOutputData;
 
-	procedure waitCycles(constant number : integer)	
-		Wait for ((ClkPeriod * 2)*number)
+	procedure waitCycles(constant number : integer)	is
+	begin
+		wait for ((ClkPeriod)*number)*ns;
 	
-	end hready_n; 
+	end waitCycles; 
 
 	variable addr_v  			: std_logic_vector(pAddrWidth-1 downto 0);
 	variable ready_v 			: std_logic;
@@ -77,39 +80,40 @@ architecture sim of AmbaMonitor_tb is
 
 	begin
 		log(ID_LOG_HDR, "Start simulation AmbaMonitor_tb");
-		iRST_n <= '0'
-		Wait for (ClkPeriod * 2) * ns;
-		iRST_n <= '1'
+		iRST_n <= '0';
+		wait for (ClkPeriod * 2) * ns;
+		iRST_n <= '1';
 
 		-- Actual test sequence.
 
 		-- No error
-		addr_v 		:= x"10000008"
-		ready_v		:= '1'
-		response_v	:= "01"
+		addr_v 		:= x"10000008";
+		ready_v		:= '1';
+		response_v	:= "01";
 		ahbSlvOutputData(addr_v, ready_v, response_v);
-		waitCycles(3)
+		waitCycles(3);
 		-- No error
-		addr_v 		:= x"10000012"
-		ready_v		:= '1'
-		response_v	:= "01"
+		addr_v 		:= x"10000012";
+		ready_v		:= '1';
+		response_v	:= "01";
 		ahbSlvOutputData(addr_v, ready_v, response_v);
-		waitCycles(5)
-		-- Error
-		addr_v 		:= x"10000016"
-		ready_v		:= '0'
-		response_v	:= "10"
+		waitCycles(5);
+		-- Error;
+		addr_v 		:= x"10000016";
+		ready_v		:= '0';
+		response_v	:= "10";
 		ahbSlvOutputData(addr_v, ready_v, response_v);
-		
-		waitCycles(1)
+
+		waitCycles(1);
 		check_value(oEAddr, x"10000012", WARNING, "Error response address check");
 		check_value(oError, '1', WARNING, "Error response message check");
 
-		report_alert_counters();
+		--report_alert_counters();
+		EndOfSimulation <= '1';
 		log(ID_LOG_HDR, "SIMULATION COMPLETED");
 		
 		report "End of simulation." severity failure;
-		Wait;
+		wait;
 
 	end process;
 
@@ -136,13 +140,12 @@ architecture sim of AmbaMonitor_tb is
  --   	);
 
 
-	I_DUT: entity work.AmbaMonitor
+	I_DUT: entity cbk.AmbaMonitor
 	generic map(
 		pwDataWidth => pwDataWidth,		
 		pAddrWidth 	=> pAddrWidth,	
-		pIndex     	=> pIndex,	
-		pMask      	=> pMask      	
-		);
+		pIndex     	=> pIndex    	
+		)
 	port map(
 		iCLK   		=> iCLK,		
 		iRST_n 		=> iRST_n,		
